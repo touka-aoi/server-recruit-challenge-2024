ac24c5b9935c1897b1b12223e794c12cb840eb16
touka_aoi <toukaseisei@gmail.com>
2023-03-29 20:48:10 +0900

    可読性向上

--- api/router_test.go
+++ api/router_test.go
@@ -102,7 +102,7 @@ func TestAlbumGet(t *testing.T) {
 	}
 
 	// 負数の場合 404 not found が返る
-	req, err = http.NewRequest("GET", "/albums-1", nil)
+	req, err = http.NewRequest("GET", "/albums/-1", nil)
 
 	// 作成に失敗した場合
 	if err != nil {
@@ -124,323 +124,384 @@ func TestAlbumGet(t *testing.T) {
 // POSTリクエストはリクエストデータを登録することのみを行う
 // バリデーションなどはServiceレベルで行う
 func TestAlbumPost(t *testing.T) {
-	// ルーターを作成
-	r := api.NewRouter()
 
 	// 重複しないIDかつ、存在するSingerIDの場合 //
+	t.Run("UniqueID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
 
-	// リクエストを作成
-	parm := `{"id": 4, "title":"Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body := []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody := bytes.NewBuffer(body)
+		// リクエストを作成
+		parm := `{"id": 4, "title":"Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// HTTPリクエストを作成
-	req, err := http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr := httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
+		// レスポンスのステータスコード(200)を確認
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
 
-	// レスポンスのボディを確認
-	log.Print("TEST1 正常POST: " + rr.Body.String())
+		expected := &model.Album{ID: 4, Title: "Alice's 3rd Album", SingerID: 1}
 
-	expected := &model.Album{ID: 4, Title: "Alice's 3rd Album", SingerID: 1}
+		var albums *model.Album
+		// レスポンスのボディを確認
+		if err := json.NewDecoder(rr.Body).Decode(&albums); err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスのボディを確認
+		assert.Equal(t, expected, albums)
 
-	var albums *model.Album
-	// レスポンスのボディを確認
-	if err := json.NewDecoder(rr.Body).Decode(&albums); err != nil {
-		t.Fatal(err)
-	}
-
-	// レスポンスのボディを確認
-	assert.Equal(t, expected, albums)
+		// レスポンスのボディを確認
+		log.Print("TEST1 正常POST: " + rr.Body.String())
+	})
 
 	// IDが重複した場合はそのまま上書きをする //
+	t.Run("Overwrite", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 1, "title":"Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// リクエストを作成
-	parm = `{"id": 1, "title":"Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// レスポンスのステータスコード(200)を確認
+		if status := rr.Code; status != http.StatusOK {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
+		}
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusOK {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusOK)
-	}
+		expected := &model.Album{ID: 1, Title: "Alice's 3rd Album", SingerID: 1}
 
-	// レスポンスのボディを確認
-	log.Print("TEST2 上書きPOST: " + rr.Body.String())
+		var albums *model.Album
+		// レスポンスのボディを確認
+		if err := json.NewDecoder(rr.Body).Decode(&albums); err != nil {
+			t.Fatal(err)
+		}
 
-	expected = &model.Album{ID: 1, Title: "Alice's 3rd Album", SingerID: 1}
+		// レスポンスのボディを確認
+		assert.Equal(t, expected, albums)
 
-	// レスポンスのボディを確認
-	if err := json.NewDecoder(rr.Body).Decode(&albums); err != nil {
-		t.Fatal(err)
-	}
+		// レスポンスのボディを確認
+		log.Print("TEST2 上書きPOST: " + rr.Body.String())
+	})
 
 	// IDが文字列の場合はエラーを返す //
+	t.Run("StringID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": "1", "title": "Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// リクエストを作成
-	parm = `{"id": "1", "title": "Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		log.Print("TEST3 タイプエラー Code: ", rr.Code, " ", rr.Body.String())
 
-	log.Print("TEST3 タイプエラー Code: ", rr.Code, " ", rr.Body.String())
+		assert.Equal(t, rr.Code, 400)
 
-	// IDが0の場合はエラーを返す //
+	})
 
-	// リクエストを作成
-	parm = `{"id": 0, "title": "Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+	// // IDが0の場合はエラーを返す //
+	t.Run("ZeroID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 0, "title": "Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	log.Print("TEST4 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		log.Print("TEST4 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+
+		assert.Equal(t, rr.Code, 400)
+	})
 
 	// titleが空の場合はエラーを返す
+	t.Run("EmptyTitle", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 0, "title": "","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// リクエストを作成
-	parm = `{"id": 0, "title": "","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		log.Print("TEST5 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
 
-	log.Print("TEST5 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		assert.Equal(t, rr.Code, 400)
 
-	// titleが文字列以外の場合はエラーを返す
-	// リクエストを作成
-	parm = `{"id": 1, "title": 11111,"singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+	})
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+	// // titleが文字列でない場合はエラーを返す
+	t.Run("StringTitle", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 1, "title": 11111,"singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	log.Print("TEST6 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+
+		log.Print("TEST6 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+
+		assert.Equal(t, rr.Code, 400)
+
+	})
 
 	// IDがnullの場合エラーを返す
-	// リクエストを作成
-	parm = `{"id": null, "title":"Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+	t.Run("NullID", func(t *testing.T) {
+		// ルータの作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": null, "title":"Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	log.Print("TEST7 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		log.Print("TEST7 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+
+		assert.Equal(t, rr.Code, 400)
+
+	})
 
 	// リクエストボディが不足している場合エラーを返す
-	// リクエストを作成
-	parm = `{}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+	t.Run("LackOfRequestBody", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	log.Print("TEST8 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		log.Print("TEST8 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
 
-	// リクエストを作成
-	parm = `{"id": 1, "title":"Alice's 3rd Album"}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+		assert.Equal(t, rr.Code, 400)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+	})
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+	// SingerIDがないパターン
+	t.Run("LackOfSingerID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 1, "title":"Alice's 3rd Album"}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	log.Print("TEST9 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// リクエストを作成
-	parm = `{"title":"Alice's 3rd Album","singer_id":1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		log.Print("TEST9 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		assert.Equal(t, rr.Code, 400)
 
-	log.Print("TEST10 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+	})
 
-	// リクエストを作成
-	parm = `{"id": 1}`
-	// JSON文字列をバイトスライスに変換
-	body = []byte(parm)
-	// io.Reader型のオブジェクトを作成
-	requestBody = bytes.NewBuffer(body)
+	// IDがないパターン
+	t.Run("LackOfID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"title":"Alice's 3rd Album","singer_id":1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
 
-	// HTTPリクエストを作成
-	req, err = http.NewRequest("POST", "/albums", requestBody)
-	// 作成に失敗した場合
-	if err != nil {
-		t.Fatal(err)
-	}
-	// レスポンスを用意
-	rr = httptest.NewRecorder()
-	// ルーターにリクエストを送信
-	r.ServeHTTP(rr, req)
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
 
-	// レスポンスのステータスコードを確認
-	if status := rr.Code; status != http.StatusBadRequest {
-		t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
-	}
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
 
-	log.Print("TEST11 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+		log.Print("TEST10 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
 
-	// t.Fatal("TestAlbumsPost: ここまで到達したらエラー")
+		assert.Equal(t, rr.Code, 400)
+
+	})
+
+	// 2つないパターン
+	t.Run("LackOfTitleAndSingerID", func(t *testing.T) {
+		// ルーターを作成
+		r := api.NewRouter()
+		// リクエストを作成
+		parm := `{"id": 1}`
+		// JSON文字列をバイトスライスに変換
+		body := []byte(parm)
+		// io.Reader型のオブジェクトを作成
+		requestBody := bytes.NewBuffer(body)
+
+		// HTTPリクエストを作成
+		req, err := http.NewRequest("POST", "/albums", requestBody)
+		// 作成に失敗した場合
+		if err != nil {
+			t.Fatal(err)
+		}
+		// レスポンスを用意
+		rr := httptest.NewRecorder()
+		// ルーターにリクエストを送信
+		r.ServeHTTP(rr, req)
+
+		// レスポンスのステータスコードを確認
+		if status := rr.Code; status != http.StatusBadRequest {
+			t.Errorf("handler returned wrong status code: got %v want %v", status, http.StatusBadRequest)
+		}
+
+		log.Print("TEST11 REQUIRE Code: ", rr.Code, " ", rr.Body.String())
+
+		assert.Equal(t, rr.Code, 400)
+
+	})
 
 }
-
-// アルバムを削除する
-// Test: /albumsN delete Test
-
-// 存在する
-
-// 存在しない
